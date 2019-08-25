52b6e1477ea605162d853779c58144ac6c46c9d3
mashafi3209 <54332351+mashafi3209@users.noreply.github.com>
2019-08-25 17:19:33 -0400

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void pressTheButton(ActionEvent event){
+        System.out.println("Hello World");
+    }
+}
